e2a837307e0465f3f8690ac4a77b0897709823de
danybeltran <josuebeltran_2010@hotmail.com>
2022-05-22 23:39:07 -0600

    first commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+export {
+  Atom,
+  Filter,
+  FilterGet,
+  AtomicState,
+  atom,
+  useAtom,
+  useValue,
+  useActions,
+  useDispatch,
+  filter,
+  storage,
+  useFilter,
+  useStorage,
+} from "atomic-state"
+
+export {
+  FetcherConfig,
+  createHttpClient,
+  fetcher,
+  useFetcher,
+} from "http-react-fetcher"
